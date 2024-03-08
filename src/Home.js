97165e58b24a115d6fda8bb15df0355e495e68ad
goutham3578 <gouthamvenkat2002@gmail.com>
2024-03-08 12:01:26 +0530

    add routing

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+
+import BlogList from "./blogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading..</div>}
+            {blogs && <BlogList blogs={blogs} title='all blogs' />}
+        </div>
+    );
+}
+export default Home;
+
+
+
+const App = () => {
+    const { data: blogs, isPending ,error}=useFetch('http://localhost:8000/blogs');
+
+    return (
+        <div className="home">
+            {error && <div>{error}</div> }
+            {isPending && <div>Loading..</div>}
+            {blogs && <Bloglist />}
+        </div>
+    )
+
+}
